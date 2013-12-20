3890f3db037a4f34f11f1c15d77328d0d1d5bb1f
Ekta Grover <ekta1007@gmail.com>
2013-12-20 23:01:45 +0530

    Update reservoir_sampling.py

--- reservoir_sampling.py
+++ reservoir_sampling.py
@@ -2,19 +2,24 @@
 # Reservoir_sampling function - code from Wiki
 
 import random
-def reservoir_sampling(file_handle,SAMPLE_COUNT) :
+def reservoir_sampling(file_handle,SAMPLE_COUNT):
         # To force the value of the seed so the results are repeatable
         #random.seed(12345)
  
-        sample_lines = []
-        for index, line in enumerate(open(file_handle)):
+        sample_titles = []
+        for index, line in enumerate(file_handle):
                 # Generate the reservoir
                 if index < SAMPLE_COUNT:
-                        sample_lines.append(line)
+                        sample_titles.append(line)
                 else:                  
                         # Randomly replace elements in the reservoir with a decreasing probability         
                         # Choose an integer between 0 and index (inclusive)               
                         r = random.randint(0, index)               
                         if r < SAMPLE_COUNT:                       
-                                sample_lines[r] = line
-        print sample_lines
+                                sample_titles[r] = line
+        # now storing the sample lines in a file
+        with open('C:/Users/Ekta.Grover/Desktop/train_logs_sampled.txt', 'w') as f:
+                for i in range(0,SAMPLE_COUNT):
+                        f.write(str(sample_titles[i]))
+# usage : reservoir_sampling(file_handle,SAMPLE_COUNT)
+
